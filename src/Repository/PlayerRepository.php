ca7dc801607b50d71cdacb7786cbcb66f3129ef7
lunarstrain1982 <artur@buildstaff.ie>
2018-07-26 15:44:32 +0100

    Created migrations for the Player entity class

--- src/Repository/PlayerRepository.php
+++ src/Repository/PlayerRepository.php
@@ -9,46 +9,16 @@
 namespace App\Repository;
 
 use App\Entity\Player;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
 
-class PlayerRepository
+class PlayerRepository extends ServiceEntityRepository
 {
-    private $players = [];
-
     /**
      * PlayerRepository constructor.
+     * @param RegistryInterface $registry
      */
-    public function __construct() {
-        $id = 1;
-        $player1 = new Player($id, 'Artur', 'Sukiennik', 36, 'Polish', 'Pogon Lebork');
-        $this->players[$id] = $player1;
-
-        $id = 2;
-        $player2 = new Player($id, 'Cristiano', 'Ronaldo', 34, 'Portuguese', 'Juventus');
-        $this->players[$id] = $player2;
-
-        $id = 3;
-        $player3 = new Player($id, 'Leo', 'Messi', 32, 'Argentinian', 'Fc Barcelona');
-        $this->players[$id] = $player3;
-    }
-
-    /**
-     * Retrieve the list of all PlayerRepository's players
-     * @return array
-     */
-    public function findAll() {
-        return $this->players;
-    }
-
-    /**
-     * Finds one Player by Player Id
-     * @param $id
-     * @return mixed|null
-     */
-    public function find($id) {
-        if (array_key_exists($id, $this->players)) {
-            return $this->players[$id];
-        } else {
-            return null;
-        }
+    public function __construct(RegistryInterface $registry) {
+        parent::__construct($registry, Player::class);
     }
 }
