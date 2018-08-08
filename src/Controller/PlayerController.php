64510939abdcbb72d587c44a4a0c82a0161c1f5d
lunarstrain1982 <artur@buildstaff.ie>
2018-08-08 16:55:37 +0100

    [1] New form added to create a new Player. [2] Player controller updated with the method responsible for processing the creation of a new Player.

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -5,16 +5,71 @@ namespace App\Controller;
 use App\Entity\Player;
 use App\Repository\PlayerRepository;
 use Doctrine\ORM\EntityManager;
-use http\Env\Request;
+//use http\Env\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PlayerController extends Controller
 {
+    /**
+     * Redirects to the form in order to create a new Player
+     * @Route("/player/createNewPlayer", name="player_create_new")
+     */
+    public function createNewPlayerAction() {
+        $args_array = [];
+        $template = 'player/newPlayer.html.twig';
+
+        return $this->render($template, $args_array);
+    }
 
     /**
-     * It lists details of the one selected Player
+     * It inserts a new Player to the DB
+     * @Route("player/create", name="player_create")
+     * @param $firstName
+     * @param $surname
+     * @param $age
+     * @param $nationality
+     * @param $clubName
+     * @return Response
+     */
+    public function createAction($firstName, $surname, $age, $nationality, $clubName) {
+        $player = new Player();
+        $player->setFirstName($firstName);
+        $player->setSurname($surname);
+        $player->setAge($age);
+        $player->setNationality($nationality);
+        $player->setClubName($clubName);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($player);
+        $em->flush();
+
+        return $this->redirectToRoute('players_list');
+        //return $this->redirectToRoute('player_show', ['id' => $player->getId()]);
+        //return new Response('New Player with id ' . $player->getId() . ' successfully created!');
+    }
+
+    /**
+     * It handles the creation of a new Player
+     * @Route("player/processCreateNewPlayer", name="process__player_create_new")
+     * @param Request $request
+     * @return Response
+     */
+    public function processCreateNewPlayerAction(Request $request) {
+        // Retrieve values from the HTTP POST data (from user form)
+        $firstName = $request->request->get('playerFirstName');
+        $surname = $request->request->get('playerSurname');
+        $age = $request->request->get('playerAge');
+        $nationality = $request->request->get('playerNationality');
+        $clubName = $request->request->get('playerClubName');
+
+        return $this->createAction($firstName, $surname, $age, $nationality, $clubName);
+    }
+
+    /**
+     * It lists details of the selected Player
      * @Route("/player/{id}", name="player_show")
      * @param Player $player
      * @return Response
@@ -52,7 +107,7 @@ class PlayerController extends Controller
 
     /**
      * It shows the list of all Players
-     * @Route("/players", name="players_list")
+     * @Route("/player", name="players_list")
      * @return Response
      */
     public function listAction() {
@@ -88,35 +143,6 @@ class PlayerController extends Controller
         return new Response('Player with id ' . $id . ' successfully deleted!');
     }
 
-    /**
-     * It creates a new Player
-     * @Route("/create", name="player_create")
-     * @return Response
-     */
-    public function createAction() {
-        $player = new Player();
-        $player->setFirstName("Mohamed");
-        $player->setSurname("Salah");
-        $player->setAge(26);
-        $player->setNationality("Egypt");
-        $player->setClubName("Liverpool");
 
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($player);
-        $em->flush();
 
-        return $this->redirectToRoute('players_list');
-        //return $this->redirectToRoute('player_show', ['id' => $player->getId()]);
-        //return new Response('New Player with id ' . $player->getId() . ' successfully created!');
-    }
-
-    /**
-     * @Route("/player_new", name="player_create_new")
-     */
-    public function createNewPlayerAction() {
-        $args_array = [];
-        $template = 'player/newPlayer.html.twig';
-
-        return $this->render($template, $args_array);
-    }
 }
